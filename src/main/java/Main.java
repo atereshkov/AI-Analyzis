b55ff38316854388b642dc0d7f65c812ef19fc18
Alexander Tereshkov <alexander.tereshkov@exposit.com>
2018-02-28 00:21:58 +0300

    Reverse map results

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import IO.FileReaderI;
 import IO.FileReaderImpl;
 
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -40,7 +41,7 @@ public class Main {
         Map<String, Float> unsortedMap = featureSelection.convertToHashMap(features, coeffs);
 
         Map<String, Float> sortedMap = unsortedMap.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
                         (e1, e2) -> e1, LinkedHashMap::new));
 
